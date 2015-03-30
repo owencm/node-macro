a50a71c9d9e25cbf5a95ba185ca1818525e38707
Owen Campbell-Moore <owencmoore@gmail.com>
2015-03-29 17:07:49 -0700

    Refactored to use generators and promises

--- macro.js
+++ macro.js
@@ -11,67 +11,102 @@ var init = function() {
 }
 
 var getMousePos = function() {
-    var ourEvent = $.CGEventCreate(null); 
-    var pos = $.CGEventGetLocation(ourEvent);
-    return {x: pos.x << 0, y: pos.y << 0}
+    return new Promise(function(resolve, reject){
+        var ourEvent = $.CGEventCreate(null); 
+        var pos = $.CGEventGetLocation(ourEvent);
+        resolve({x: pos.x << 0, y: pos.y << 0});
+    });
 }
 
 var setMouse = function(x, y) {
-    var e = $.CGEventCreateMouseEvent(null, $.kCGEventMouseMoved, $.CGPointMake(x, y), $.kCGMouseButtonLeft);
-    $.CGEventPost($.kCGHIDEventTap, e);
-    // $.CFRelease(e);
+    return new Promise(function(resolve, reject){
+        var e = $.CGEventCreateMouseEvent(null, $.kCGEventMouseMoved, $.CGPointMake(x, y), $.kCGMouseButtonLeft);
+        $.CGEventPost($.kCGHIDEventTap, e);
+        // $.CFRelease(e);
+        resolve();
+    });
 }
 
 var mouseDown = function(x, y, right) {
-    if (right == undefined) {
-        right = false;
-    }
-    var kCGEvent = right ? $.kCGEventRightMouseDown : $.kCGEventLeftMouseDown;
-    var kCGMouseButton = right ? $.kCGMouseButtonRight : $.kCGMouseButtonLeft;
-    var e = $.CGEventCreateMouseEvent(null, kCGEvent, $.CGPointMake(x, y), kCGMouseButton);
-    $.CGEventPost($.kCGHIDEventTap, e);
-    // $.CFRelease(e);
+    return new Promise(function(resolve, reject){
+        if (right == undefined) {
+            right = false;
+        }
+        var kCGEvent = right ? $.kCGEventRightMouseDown : $.kCGEventLeftMouseDown;
+        var kCGMouseButton = right ? $.kCGMouseButtonRight : $.kCGMouseButtonLeft;
+        var e = $.CGEventCreateMouseEvent(null, kCGEvent, $.CGPointMake(x, y), kCGMouseButton);
+        $.CGEventPost($.kCGHIDEventTap, e);
+        // $.CFRelease(e);
+        resolve();
+    });
 }
 
 var mouseUp = function(x, y, right) {
-    if (right == undefined) {
-        right = false;
-    }
-    var kCGEvent = right ? $.kCGEventRightMouseUp : $.kCGEventLeftMouseUp;
-    var kCGMouseButton = right ? $.kCGMouseButtonRight : $.kCGMouseButtonLeft;
-    var e = $.CGEventCreateMouseEvent(null, kCGEvent, $.CGPointMake(x, y), kCGMouseButton);
-    $.CGEventPost($.kCGHIDEventTap, e);
-    // $.CFRelease(e);
+    return new Promise(function(resolve, reject){
+        if (right == undefined) {
+            right = false;
+        }
+        var kCGEvent = right ? $.kCGEventRightMouseUp : $.kCGEventLeftMouseUp;
+        var kCGMouseButton = right ? $.kCGMouseButtonRight : $.kCGMouseButtonLeft;
+        var e = $.CGEventCreateMouseEvent(null, kCGEvent, $.CGPointMake(x, y), kCGMouseButton);
+        $.CGEventPost($.kCGHIDEventTap, e);
+        // $.CFRelease(e);
+        resolve();
+    });
 }
 
 var jsCharToMacKeyCode = function (key) {
-    var map = {'a': 0, 'b': 11, 'c': 8, 'd': 2, 'e': 14, 'f': 3, 'g': 5, 'h': 4, 'i': 34, 'j': 38, 'k': 40, 'l': 37, 'm': 46, 'n': 45, 'o': 31, 'p': 35, 'q': 12, 'r': 15, 's': 1, 't': 17, 'u': 32, 'v': 9, 'w': 13, 'x': 7, 'y': 16, 'z': 6};
+    var map = {'a': 0, 'b': 11, 'c': 8, 'd': 2, 'e': 14, 'f': 3, 'g': 5, 'h': 4, 'i': 34, 'j': 38, 'k': 40, 'l': 37, 'm': 46, 'n': 45, 'o': 31, 'p': 35, 'q': 12, 'r': 15, 's': 1, 't': 17, 'u': 32, 'v': 9, 'w': 13, 'x': 7, 'y': 16, 'z': 6, ' ': 49, 'shift': 56};
     return map[key];
 }
 
 // Keycodes defined in https://gist.github.com/willwade/5330474
 var keyDown = function(char) {
-    var keyCode = jsCharToMacKeyCode(char);
-    if (keyCode == undefined) {
-        throw 'Unrecognised character in keyDown';
-    }
-    var e = $.CGEventCreateKeyboardEvent(null, keyCode, true);
-    $.CGEventPost($.kCGHIDEventTap, e);
-    // $.CFRelease(e);
+    return new Promise(function(resolve, reject){
+        var keyCode = jsCharToMacKeyCode(char);
+        if (keyCode == undefined) {
+            throw 'Unrecognised character in keyDown';
+        }
+        var e = $.CGEventCreateKeyboardEvent(null, keyCode, true);
+        $.CGEventPost($.kCGHIDEventTap, e);
+        // $.CFRelease(e);
+        resolve();
+    });
 }
 
 var keyUp = function(char) {
-    var keyCode = jsCharToMacKeyCode(char);
-    if (keyCode == undefined) {
-        throw 'Unrecognised character in keyDown';
-    }
-    var e = $.CGEventCreateKeyboardEvent(null, keyCode, false);
-    $.CGEventPost($.kCGHIDEventTap, e);
-    // $.CFRelease(e);
+    return new Promise(function(resolve, reject){
+        var keyCode = jsCharToMacKeyCode(char);
+        if (keyCode == undefined) {
+            throw 'Unrecognised character in keyUp';
+        }
+        var e = $.CGEventCreateKeyboardEvent(null, keyCode, false);
+        $.CGEventPost($.kCGHIDEventTap, e);
+        // $.CFRelease(e);
+        resolve();
+    });
 }
 
 var getDisplayId = function() {
     return $.CGMainDisplayID();
+}
+
+var getColor = function (x, y) {
+    return new Promise(function(resolve, reject){
+        var displayID = getDisplayId();
+        var cGImageRef = $.CGDisplayCreateImageForRect(displayID, $.CGRectMake(x, y, 1, 1));
+        var width = $.CGImageGetWidth(cGImageRef);
+        var height = $.CGImageGetHeight(cGImageRef);
+        var data = new Buffer(height * width * 4);
+        var bytesPerPixel = 4;
+        var bytesPerRow = bytesPerPixel * width;
+        var bitsPerComponent = 8;
+        var cGColorSpaceRef = $.CGColorSpaceCreateDeviceRGB();
+        var cGContextRef = $.CGBitmapContextCreate(data, width, height, bitsPerComponent, bytesPerRow, cGColorSpaceRef, $.kCGImageAlphaPremultipliedLast | $.kCGBitmapByteOrder32Big);
+        $.CGContextDrawImage(cGContextRef, $.CGRectMake(0, 0, width, height), cGImageRef);
+        $.CGContextRelease(cGContextRef);
+        resolve({r: data[0], g: data[1], b: data[2]});
+    });
 }
 
 var getBitmap = function(xs, ys, xe, ye) {
@@ -82,30 +117,16 @@ var getBitmap = function(xs, ys, xe, ye) {
     return bitmap;
 }
 
-var getColor = function (x, y) {
-    var displayID = getDisplayId();
-    var cGImageRef = $.CGDisplayCreateImageForRect(displayID, $.CGRectMake(x, y, 1, 1));
-    var width = $.CGImageGetWidth(cGImageRef);
-    var height = $.CGImageGetHeight(cGImageRef);
-    var data = new Buffer(height * width * 4);
-    var bytesPerPixel = 4;
-    var bytesPerRow = bytesPerPixel * width;
-    var bitsPerComponent = 8;
-    var cGColorSpaceRef = $.CGColorSpaceCreateDeviceRGB();
-    var cGContextRef = $.CGBitmapContextCreate(data, width, height, bitsPerComponent, bytesPerRow, cGColorSpaceRef, $.kCGImageAlphaPremultipliedLast | $.kCGBitmapByteOrder32Big);
-    $.CGContextDrawImage(cGContextRef, $.CGRectMake(0, 0, width, height), cGImageRef);
-    $.CGContextRelease(cGContextRef);
-    return {r: data[0], g: data[1], b: data[2]};    
-}
-
 var getRealColor = function(x, y) {
-    var nSBitmapImageRep = getBitmap(x, y, x, y);
-    var nSColor = nSBitmapImageRep('colorAtX', 0, 'y', 0);  
-    nSBitmapImageRep('release');
-    var red = nSColor('redComponent') * 255;
-    var green = nSColor('greenComponent') * 255;
-    var blue = nSColor('blueComponent') * 255;
-    return {r: red, g: green, b: blue};    
+    return new Promise(function(resolve, reject){
+        var nSBitmapImageRep = getBitmap(x, y, x, y);
+        var nSColor = nSBitmapImageRep('colorAtX', 0, 'y', 0);  
+        nSBitmapImageRep('release');
+        var red = nSColor('redComponent') * 255;
+        var green = nSColor('greenComponent') * 255;
+        var blue = nSColor('blueComponent') * 255;
+        resolve({r: red, g: green, b: blue});
+    });
 }
 
 var findColor = function(target, xs, ys, xe, ye) {
@@ -113,37 +134,39 @@ var findColor = function(target, xs, ys, xe, ye) {
 }
 
 var findColorTolerance = function(target, xs, ys, xe, ye, tol) {
-    var startTime = Date.now();
-    var displayID = getDisplayId();
-    var cGImageRef = $.CGDisplayCreateImageForRect(displayID, $.CGRectMake(xs, ys, xe - xs + 1, ye - ys + 1));
-    var width = $.CGImageGetWidth(cGImageRef);
-    var height = $.CGImageGetHeight(cGImageRef);
-    var data = new Buffer(height * width * 4);
-    var bytesPerPixel = 4;
-    var bytesPerRow = bytesPerPixel * width;
-    var bitsPerComponent = 8;
-    var cGColorSpaceRef = $.CGColorSpaceCreateDeviceRGB();
-    var cGContextRef = $.CGBitmapContextCreate(data, width, height, bitsPerComponent, bytesPerRow, cGColorSpaceRef, $.kCGImageAlphaPremultipliedLast | $.kCGBitmapByteOrder32Big);
-    $.CGContextDrawImage(cGContextRef, $.CGRectMake(0, 0, width, height), cGImageRef);
-    $.CGContextRelease(cGContextRef);
-    // console.log(data.toJSON());
-    for (var y = 0; y <= 2 * (ye - ys); y += 2) {
-        for (var x = 0; x <= 2 * (xe - xs); x += 2) {
-            var r = data[4*(x + y * width)];
-            var g = data[4*(x + y * width) + 1];
-            var b = data[4*(x + y * width) + 2];
-            // console.log('red is at '+4*(x + y * width)+', green is at '+4*(x + y * width + 1)+', blue is at '+4*(x + y * width + 2));
-            // console.log('color at '+(xs + x/2)+', '+(ys + y/2) + ' is '+r+', '+g+', '+b);
-            if (Math.abs(r - target.r) + Math.abs(g - target.g) + Math.abs(b - target.b) <= tol) {
-                // setMouse(xs + x/2, ys + y/2);
-                return {x: xs + x/2, y: ys + y/2};
+    return new Promise(function(resolve, reject){
+        var startTime = Date.now();
+        var displayID = getDisplayId();
+        var cGImageRef = $.CGDisplayCreateImageForRect(displayID, $.CGRectMake(xs, ys, xe - xs + 1, ye - ys + 1));
+        var width = $.CGImageGetWidth(cGImageRef);
+        var height = $.CGImageGetHeight(cGImageRef);
+        var data = new Buffer(height * width * 4);
+        var bytesPerPixel = 4;
+        var bytesPerRow = bytesPerPixel * width;
+        var bitsPerComponent = 8;
+        var cGColorSpaceRef = $.CGColorSpaceCreateDeviceRGB();
+        var cGContextRef = $.CGBitmapContextCreate(data, width, height, bitsPerComponent, bytesPerRow, cGColorSpaceRef, $.kCGImageAlphaPremultipliedLast | $.kCGBitmapByteOrder32Big);
+        $.CGContextDrawImage(cGContextRef, $.CGRectMake(0, 0, width, height), cGImageRef);
+        $.CGContextRelease(cGContextRef);
+        // console.log(data.toJSON());
+        for (var y = 0; y <= 2 * (ye - ys); y += 2) {
+            for (var x = 0; x <= 2 * (xe - xs); x += 2) {
+                var r = data[4*(x + y * width)];
+                var g = data[4*(x + y * width) + 1];
+                var b = data[4*(x + y * width) + 2];
+                // console.log('red is at '+4*(x + y * width)+', green is at '+4*(x + y * width + 1)+', blue is at '+4*(x + y * width + 2));
+                // console.log('color at '+(xs + x/2)+', '+(ys + y/2) + ' is '+r+', '+g+', '+b);
+                if (Math.abs(r - target.r) + Math.abs(g - target.g) + Math.abs(b - target.b) <= tol) {
+                    // setMouse(xs + x/2, ys + y/2);
+                    resolve({x: xs + x/2, y: ys + y/2});
+                }
             }
         }
-    }
-    var endTime = Date.now();
-    var timeDelta = endTime-startTime;
-    console.log('Done. Took '+timeDelta+'ms, or '+(timeDelta/((xe-xs)*(ye-ys)))+'ms per pixel.');
-    return {x: -1, y: -1}
+        var endTime = Date.now();
+        var timeDelta = endTime-startTime;
+        console.log('Done. Took '+timeDelta+'ms, or '+(timeDelta/((xe-xs)*(ye-ys)))+'ms per pixel.');
+        resolve({x: -1, y: -1});
+    });
 }
 
 
--- main.js
+++ main.js
@@ -1,114 +1,154 @@
 var m = require('./macro.js');
 
+function spawn(generatorFunc) {
+  function continuer(verb, arg) {
+    var result;
+    try {
+      result = generator[verb](arg);
+    } catch (err) {
+      return Promise.reject(err);
+    }
+    if (result.done) {
+      return result.value;
+    } else {
+      return Promise.resolve(result.value).then(onFulfilled, onRejected);
+    }
+  }
+  var generator = generatorFunc();
+  var onFulfilled = continuer.bind(continuer, "next");
+  var onRejected = continuer.bind(continuer, "throw");
+  return onFulfilled();
+}
+
 var random = function (from, to) {
 	return from + (Math.random() * (to - from) + 0.5) << 0;
 }
 
+var wait = function (time) {
+	return new Promise(function(resolve, reject){
+		setTimeout(resolve, time);
+	});
+}
+
 var clickMouse = function (x, y) {
 	return new Promise(function (resolve, reject) {
-		m.mouseDown(x, y);
-		setTimeout(function () {
-			m.mouseUp(x, y);
+		spawn(function*() {
+			yield m.mouseDown(x, y);
+			yield wait(random(1, 20));
+			yield m.mouseUp(x, y);
 			resolve();
-		}, random(1, 20));
+		})
 	});
 }
 
 var moveMouse = function (endX, endY) {
 	return new Promise(function(resolve, reject) {
-		var currPos = m.getMousePos();
-		var currX = currPos.x;
-		var currY = currPos.y;
-		var bigDX = endX - currX;
-		var bigDY = endY - currY;
-		var time = Math.sqrt(Math.pow(bigDX, 2) + Math.pow(bigDY, 2)) << 0;
-		var moveEveryX = time/bigDX;
-		var moveEveryY = time/bigDY;
-		var movingRight = (moveEveryX > 0) ? 1 : -1;
-		var movingDown = (moveEveryY > 0) ? 1 : -1;
-		moveEveryX = Math.abs(moveEveryX);
-		moveEveryY = Math.abs(moveEveryY);
-		var xCounter = 0;
-		var yCounter = 0;
-		var interval = setInterval(function () {
-			if (currX == endX && currY == endY) {
-				clearInterval(interval);
-				resolve();
-			} else {
-				xCounter++;
-				yCounter++;
-				if (xCounter >= moveEveryX) {
-					xCounter -= moveEveryX;
-					currX += movingRight;
+		spawn(function*(){
+			var currPos = yield m.getMousePos();
+			var currX = currPos.x;
+			var currY = currPos.y;
+			var bigDX = endX - currX;
+			var bigDY = endY - currY;
+			var time = Math.sqrt(Math.pow(bigDX, 2) + Math.pow(bigDY, 2)) << 0;
+			var moveEveryX = time/bigDX;
+			var moveEveryY = time/bigDY;
+			var movingRight = (moveEveryX > 0) ? 1 : -1;
+			var movingDown = (moveEveryY > 0) ? 1 : -1;
+			moveEveryX = Math.abs(moveEveryX);
+			moveEveryY = Math.abs(moveEveryY);
+			var xCounter = 0;
+			var yCounter = 0;
+			while (true) {
+				if (currX == endX && currY == endY) {
+					resolve();
+					break;
+				} else {
+					xCounter++; 
+					yCounter++;
+					if (xCounter >= moveEveryX) {
+						xCounter -= moveEveryX;
+						currX += movingRight;
+					}
+					if (yCounter >= moveEveryY) {
+						yCounter -= moveEveryY;
+						currY += movingDown;
+					}
+					yield m.setMouse(currX, currY);
+					yield wait(1);
 				}
-				if (yCounter >= moveEveryY) {
-					yCounter -= moveEveryY;
-					currY += movingDown;
-				}
-				m.setMouse(currX, currY);
 			}
-		}, 1);
+		});
 	});
 }
 
 var type = function (str) {
 	return new Promise(function(resolve, reject) {
-		var i = 0;
-		var typeKey = function (key) {
-			m.keyDown(key);
-			setTimeout(function () {
-				m.keyUp(key);
-				setTimeout(function() {
-					if (i < str.length) {
-						typeKey(str[i++]);
-					} else {
-						resolve();
-					}
-				}, random(30, 70))
-			}, random(2, 20));
-		}
-		typeKey(str[i++]);
+		spawn(function*() {
+			for(var i = 0; i < str.length; i++) {
+				var key = str[i];
+				var uppercase = key !== key.toLowerCase();
+				if (uppercase) { 
+					key = key.toLowerCase();
+					yield m.keyDown('shift'); 
+					yield wait(random(5,20)); 
+				};
+				yield m.keyDown(key);
+				yield wait(random(5, 20));
+				yield m.keyUp(key);
+				yield wait(random(40, 80));
+				if (uppercase) { 
+					yield m.keyUp('shift'); 
+					yield wait(random(5,20)); 
+				};
+			}
+			resolve();
+		});
 	});
 }
 
-m.init();
+spawn(function*(){
+	try{
+		m.init();
+		console.log('Started');
+		yield moveMouse(150, 120);
+		yield wait(100);
+		yield clickMouse(150, 120);
+		yield wait(100);
+		yield type('Hello world');
+	} catch(err) {
+		console.log(err);
+	}
 
-var target = {r: 255, g: 154, b: 0};
-console.log('Searching for ',target);
-var pos = m.findColorTolerance(target, 0, 0, 1000, 1000, 40);
-console.log(pos);
-if (pos.x > -1 && pos.y > -1) {
-	m.setMouse(pos.x, pos.y);
-	console.log('The color there is:' + JSON.stringify(m.getColor(pos.x, pos.y)));
-}
+	// var target = {r: 203, g: 10, b: 77};
+	// console.log('Searching for ',target);
+	// var pos = m.findColorTolerance(target, 0, 0, 1000, 1000, 0);
+	// console.log(pos);
+	// if (pos.x > -1 && pos.y > -1) {
+	// 	m.setMouse(pos.x, pos.y);
+	// }
 
-// for (var x = 329; x < 350; x++) {
-// 	console.log(m.processScreen(x, 466, x, 466));
-// 	console.log(m.getColor(x, 466));
-// 	m.setMouse(x, 466);
-// }
+	// for (var x = 329; x < 350; x++) {
+	// 	console.log(m.processScreen(x, 466, x, 466));
+	// 	console.log(m.getColor(x, 466));
+	// 	m.setMouse(x, 466);
+	// }
 
-// var y = 350;
-// for (var x = 360; x < 500; x++) {
-// 	var cola = m.processScreen(x, y, x, y);
-// 	var colb = m.getColor(x, y);
-// 	if (cola.r !== colb.r || cola.g !== colb.g || cola.b !== colb.b) {
-// 		console.log('No match at '+x+', '+y,cola,colb);
-// 	}
-// 	m.setMouse(x, y);
-// }
+	// var y = 350;
+	// for (var x = 360; x < 500; x++) {
+	// 	var cola = m.processScreen(x, y, x, y);
+	// 	var colb = m.getColor(x, y);
+	// 	if (cola.r !== colb.r || cola.g !== colb.g || cola.b !== colb.b) {
+	// 		console.log('No match at '+x+', '+y,cola,colb);
+	// 	}
+	// 	m.setMouse(x, y);
+	// }
 
-// moveMouse(390, 100).then(function() {
-// 	clickMouse(390, 100).then(function () {
-// 		console.log("Done!");
-// 	})
-// });
+	// setInterval(function () {
+	// 	var pos = m.getMousePos();
+	// 	console.log(pos);
+	// 	var color = m.getColor(pos.x,pos.y);
+	// 	console.log(color);
+	// },100);
 
-// setInterval(function () {
-// 	var pos = m.getMousePos();
-// 	console.log(pos);
-// 	var color = m.getColor(pos.x,pos.y);
-// 	console.log(color);
-// },100);
-
-m.quit();
+	m.quit();
+});
